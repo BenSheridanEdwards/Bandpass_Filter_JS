61fe53303f0962c15ae3c457981839fbc74ad5f6
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-03 16:16:18 +0100

    docs: create and add code and test screenshot

--- Bandpass/bandpass.test.js
+++ Bandpass/bandpass.test.js
@@ -2,68 +2,32 @@ const bandpass = require('./bandpass');
 
 describe('bandpass', () => {
   it('takes a soundwave array, and returns it', () => {
-    expect(bandpass([40, 60, 40, 60, 40], 80, 20)).toEqual([
-      40,
-      60,
-      40,
-      60,
-      40,
-    ]);
+    expect(bandpass([40, 60, 40, 60, 40], 80, 20)).toEqual([40, 60, 40, 60, 40]);
   });
 
   describe('when a soundwave contains a frequency that breaches the upper boundary', () => {
     it('sets a single frequency to the upper boundary', () => {
-      expect(bandpass([40, 100, 40, 60, 40], 80, 20)).toEqual([
-        40,
-        80,
-        40,
-        60,
-        40,
-      ]);
+      expect(bandpass([40, 100, 40, 60, 40], 80, 20)).toEqual([40, 80, 40, 60, 40]);
     });
 
     it('sets multiple frequencies to the upper boundary', () => {
-      expect(bandpass([40, 100, 40, 100, 40], 80, 20)).toEqual([
-        40,
-        80,
-        40,
-        80,
-        40,
-      ]);
+      expect(bandpass([40, 100, 40, 100, 40], 80, 20)).toEqual([40, 80, 40, 80, 40]);
     });
   });
 
   describe('when a soundwave contains a frequency that breaches the lower boundary', () => {
     it('sets a single frequency to the lower boundary', () => {
-      expect(bandpass([10, 60, 40, 60, 40], 80, 20)).toEqual([
-        20,
-        60,
-        40,
-        60,
-        40,
-      ]);
+      expect(bandpass([10, 60, 40, 60, 40], 80, 20)).toEqual([20, 60, 40, 60, 40]);
     });
 
     it('sets multiple frequencies to the lower boundary', () => {
-      expect(bandpass([10, 60, 10, 60, 40], 80, 20)).toEqual([
-        20,
-        60,
-        20,
-        60,
-        40,
-      ]);
+      expect(bandpass([10, 60, 10, 60, 40], 80, 20)).toEqual([20, 60, 20, 60, 40]);
     });
   });
 
   describe('when a soundwave contains frequenices that breach both the upper and lower boundary', () => {
     it('sets both frequencies to their respective boundary', () => {
-      expect(bandpass([10, 100, 40, 60, 40], 80, 20)).toEqual([
-        20,
-        80,
-        40,
-        60,
-        40,
-      ]);
+      expect(bandpass([10, 100, 40, 60, 40], 80, 20)).toEqual([20, 80, 40, 60, 40]);
     });
   });
 
